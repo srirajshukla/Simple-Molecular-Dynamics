3a3f64390a4da5c07815912b502e2bc196df2127
Gyanendra Shukla <srirajshukla@gmail.com>
2021-07-02 09:51:02 +0530

    Calling the main method from library
    Out of the three imported methods, we will start with
    mainVeletSquare.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import Lib
+import Lib (mainNewton, mainVerlet, mainVerletSquare)
+
+-- Possible Options to start simulation
+-- 1. mainNewton
+-- 2. mainVerlet
+-- 3. mainVerletSquare
+-- 
+-- mainNewton Two atoms with newtonian calculations
+-- mainVerlet Two atoms with verlet integration
+-- mainVerletSquare A lattice of atoms with verlet integration
 
 main :: IO ()
-main = someFunc
+main = mainVerletSquare
